6f4b42fe6c1cb942e7813d382209463ffeafa476
chansuke <moonset20@gmail.com>
2015-11-18 14:24:14 +0900

    Add validate implemation

--- lib/validatessn.rb
+++ lib/validatessn.rb
@@ -1,5 +1,12 @@
 require "validatessn/version"
 
 module Validatessn
-  # Your code goes here...
+  def validate_ssn(number)
+    return false unless number.length == 9
+    if number.match(/^[0-8]\d{2}-\d{2}-\d{4}$/)
+      return true
+    else
+      return false
+    end
+  end
 end
